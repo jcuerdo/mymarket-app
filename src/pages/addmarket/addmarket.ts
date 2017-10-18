73439959578781c7ab2a592d11d969b5ada15c1e
Jorge Cuerdo Álvarez <jocual@gmail.com>
2017-10-18 03:15:00 +0200

    Revert map

--- src/pages/addmarket/addmarket.ts
+++ src/pages/addmarket/addmarket.ts
@@ -6,15 +6,15 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import {Market} from '../../models/market';
 import {Photo} from '../../models/photo';
-import {ApiServiceProvider} from '../../providers/api-service/api-service';
-import {MapProvider} from '../../providers/map/map';
+import { ApiServiceProvider } from '../../providers/api-service/api-service';
+
 
 @Component({
     selector: 'page-add-market',
     templateUrl: 'addmarket.html'
 })
 export class AddMarketPage {
-    
+
     @ViewChild('map') mapElement: ElementRef;
 
     position: any;
@@ -23,6 +23,7 @@ export class AddMarketPage {
     placesService: any;
     places: any = [];
     query: string = '';
+    searchDisabled: boolean = true;
     
     market: Market;
     
@@ -35,7 +36,6 @@ export class AddMarketPage {
         public alertCtrl: AlertController,
         private camera: Camera,
         private apiProvider: ApiServiceProvider,
-        private mapProvider: MapProvider,
         
     ) {
         this.market = new Market();
@@ -43,20 +43,107 @@ export class AddMarketPage {
 
     private ionViewDidLoad(): void {
 
-        this.mapProvider.createMap(this.mapElement);
-        this.mapProvider.getPosition().then(pos => {
-            this.mapProvider.changeMapPosition(pos);
+        window['mapInit'] = () => {
+            this.initMap();
+        }
+
+        let script = document.createElement("script");
+        script.id = "googleMaps";
+
+        script.src = 'http://maps.google.com/maps/api/js?key=AIzaSyDlRrMhhZXm-uhLM6XYAa4EWKdqgDSPPQk&callback=mapInit&libraries=places';
+
+        document.body.appendChild(script);
+
+        this.geolocation.getCurrentPosition().then((position) => {
+            this.position = position;
+            if (this.map) {
+                var latLng = new google.maps.LatLng(this.position.coords.latitude, this.position.coords.longitude);
+                this.map.setCenter(latLng);
+                this.map.setZoom(15);
+            }
+        }).catch((error) => {
+            this.presentAlert('Error', error.message);            
         });
     }
 
-    searchPlace() {
-        this.mapProvider.searchPlace(this.query, this.places);
-        //mandarle un callback y pista
+    initMap() {
+        if (google || google.maps) {
+            if (this.position) {
+                var latLng = new google.maps.LatLng(this.position.coords.latitude, this.position.coords.longitude);
+                var zoom = 15;
+            } else {
+                var latLng = new google.maps.LatLng(40.423504, -3.689432);
+                var zoom = 10;
+            }
+
+            let mapOptions = {
+                center: latLng,
+                zoom: zoom,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: this.map,
+                title: ''
+            });
+
+            marker.bindTo('position', this.map, 'center');
+
+            this.autocompleteService = new google.maps.places.AutocompleteService();
+            this.placesService = new google.maps.places.PlacesService(this.map);
+            this.searchDisabled = false;
+        }
     }
 
+    searchPlace() {
+        if (this.query.length > 0 && !this.searchDisabled) {
+
+            let config = {
+                types: ['geocode'],
+                input: this.query
+            }
+
+            this.autocompleteService.getPlacePredictions(config, (predictions, status) => {
+
+                if (status == google.maps.places.PlacesServiceStatus.OK && predictions) {
+
+                    this.places = [];
+
+                    predictions.forEach((prediction) => {
+                        this.places.push(prediction);
+                    });
+                }
+
+            });
+
+        } else {
+            this.places = [];
+        }
+
+    }
+
+
     selectPlace(place) {
-        this.mapProvider.selectPlace(place);
+
         this.places = [];
+
+        let location = {
+            lat: null,
+            lng: null,
+            name: place.name
+        };
+
+        this.placesService.getDetails({ placeId: place.place_id }, (details) => {
+
+            location.name = details.name;
+            location.lat = details.geometry.location.lat();
+            location.lng = details.geometry.location.lng();
+
+            this.map.setCenter({ lat: location.lat, lng: location.lng });
+        });
+
     }
 
     saveMarket(): void {
@@ -65,10 +152,15 @@ export class AddMarketPage {
             content: '',
         });
 
-        this.market.setLat(this.mapProvider.getCenter().lat());
-        this.market.setLng(this.mapProvider.getCenter().lng());
+        var headers = new Headers();
+        headers.append("Accept", 'application/json');
+        headers.append('Content-Type', 'application/json');
+        let options = new RequestOptions({ headers: headers });
 
         loader.present().then(() => {
+
+            this.market.setLat(this.map.center.lat());
+            this.market.setLng(this.map.center.lng());
 
             this.apiProvider.saveMarket(this.market)
                 .subscribe(res => {
@@ -77,7 +169,7 @@ export class AddMarketPage {
 
                     this.market.getPhotos().forEach(function(element) {
                         if(element.getContent()){
-                            this.apiProvider.savePhoto(this.market, element)
+                            this.apiProvider.saveMarketPhoto(this.market,element)
                                 .subscribe(res => {
                                     let imgData = res.json();
                                     element.setId(imgData.id);
@@ -114,19 +206,16 @@ export class AddMarketPage {
             mediaType: this.camera.MediaType.PICTURE,
             correctOrientation: true,
           }
+
            this.camera.getPicture(options).then((imageData) => {
-               this.getPhoto(imageData, index, element);
+           let base64ImageUrl = 'data:image/jpeg;base64,' + imageData;
+           let photo = new Photo();
+           photo.setContent(base64ImageUrl);
+           this.market.addPhoto(photo,index);
+           element.srcElement.src = base64ImageUrl;
           }, (err) => {
            console.log(err);
           });
     }
 
-
-    private getPhoto(imageData: any, index: any, element: any) {
-        let base64ImageUrl = 'data:image/jpeg;base64,' + imageData;
-        let photo = new Photo();
-        photo.setContent(base64ImageUrl);
-        this.market.addPhoto(photo, index);
-        element.srcElement.src = base64ImageUrl;
-    }
 }
